812eb5162e6966b6f60c359a0eee1c96596140eb
chenze.li <chenze.li@thoughtworks.com>
2019-10-30 17:10:44 +0800

    add the ribbon and eureka

--- microservicecloud-consumer-dept-80/src/main/java/atguigu/springcloud/controller/DeptController_Consumer.java
+++ microservicecloud-consumer-dept-80/src/main/java/atguigu/springcloud/controller/DeptController_Consumer.java
@@ -1,9 +1,10 @@
 package atguigu.springcloud.controller;
 
 import atguigu.springcloud.entities.Dept;
-import lombok.Builder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -11,22 +12,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/consumer")
 public class DeptController_Consumer {
-    private static final String REST_URL_PREFIX = "http://localhost:8001";
+    private static final String REST_URL_PREFIX = "http://MICROSERVICECLOUD-DEPT";
 
     @Autowired
     private RestTemplate restTemplate;
 
-    @PostMapping("/dept/add")
+    @RequestMapping("/dept/add")
     public boolean add(Dept dept) {
-        return restTemplate.postForObject(REST_URL_PREFIX + "/dept/add", dept, Boolean.class);
+        return restTemplate.postForObject(REST_URL_PREFIX + "/dept/add", dept, boolean.class);
     }
 
-    @GetMapping("/dept/get/{id}")
+    @RequestMapping("/dept/get/{id}")
     public Dept get(@PathVariable("id") Long id) {
         return restTemplate.getForObject(REST_URL_PREFIX + "/dept/get/" + id, Dept.class);
     }
 
-    @GetMapping("/dept/list")
+    @RequestMapping("/dept/list")
     public List<Dept> list() {
         return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list", List.class);
     }
